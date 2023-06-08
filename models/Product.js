9e7d67bf8bd5d254bed664e86a7fb246784143f2
NicoMen <nicomendoza.92@gmail.com>
2023-06-08 19:10:20 +0200

    cambio en models products

--- models/Product.js
+++ models/Product.js
@@ -11,12 +11,16 @@ const ProductSchema = new Schema({
         type: Number,
         require: true,
     },
-    images:[{type:String}],
+    images: [{ type: String }],
     category: {
-        type:mongoose.Types.ObjectId, 
-        ref:'Category'
+        type: mongoose.Types.ObjectId,
+        ref: 'Category'
     },
-    properties: {type:Object},
-});
+    properties: { type: Object },
+},
+    {
+        timestamps: true,
+    }
+);
 
 export const Product = models.Product || model('Product', ProductSchema);
