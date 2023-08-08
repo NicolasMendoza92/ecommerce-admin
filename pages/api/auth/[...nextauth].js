05ca0a3cf7aff24b29ab5c7e5c2070cdb06d7222
NicoMen <nicomendoza.92@gmail.com>
2023-08-08 11:30:21 +0200

    admin emails

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,7 @@ import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth, { getServerSession } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+const adminEmails = ['nicomendoza.92@gmail.com']
 // vamos a ver si el email que se encuentra dentro de nuestra base de datos como admin.
 async function isAdminEmail(email) {
  if (await Admin.findOne({ email })){
@@ -26,7 +27,7 @@ export const authOptions = {
   // hacemos console log de session para ver que es lo que tenemos que verificar en el objeto que trae, como email , name etc.
   callbacks: {
     session: async ({ session, token, user }) => {
-      if (await isAdminEmail(session?.user?.email)) {
+      if (adminEmails.includes(session?.user?.email)) {
         return session;
       } else {
         return false;
@@ -41,7 +42,7 @@ export default NextAuth(authOptions);
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req, res, authOptions);
   // si no eres admin => pasa lo siguiente. El no lo damos con el "!"
-  if (!(await isAdminEmail(session?.user?.email))) {
+  if (!adminEmails.includes(session?.user?.email)) {
     res.status(401);
     throw 'You are not an admin';
   }
